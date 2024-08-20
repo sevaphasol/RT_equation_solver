032ea0bb66be9366c7a17f389edfff1b340cd34e
sevaphasol <sevsol2512@gmail.com>
2024-08-20 19:17:36 +0300

    Update main.c
    Добавил input и clear_std_out
    Сделал проверку вводимых значений

--- main.c
+++ main.c
@@ -1,78 +1,112 @@
 #include <stdio.h>
 #include <TXLib.h>
 #include <math.h>
+#include <stdlib.h>
 
 const int INF_ROOTS = -1; // константа для определения бесконченого кол-ва корней
-const double EPSILON = 0.000001; //константа для погрещности в сравнении double и нуля
+const double EPSILON = 0.000001; //константа для погрешности в сравнении double и нуля
+
+int is_null(double n);
+int solver(double a, double b, double c, double* x1, double* x2);
+void output(const int nRoots, const double x1, const double x2);
+void input(double* a, double* b, double* c);
+void clear_std_out();
+
+int main()
+{
+    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+
+    input(&a, &b, &c);
+
+    int nRoots = solver(a, b, c, &x1, &x2);
+
+    output(nRoots, x1, x2);
+
+    return EXIT_SUCCESS;
+}
 
 int is_null(double n)
-    {
-        return (abs(n) < EPSILON);
-    }
+{
+    return (fabs(n) < EPSILON);
+}
 
 int solver(double a, double b, double c, double* x1, double* x2) // решает квадратное уравнение
+{
+    if (is_null(a))
     {
-        if (is_null(a))
+        if (is_null(b))
         {
-            if (is_null(b))
-            {
-                if (is_null(c))
-                    return INF_ROOTS; // бесконечное кол-во корней
-                else
-                    return 0; // нет корней
-            }
+            if (is_null(c))
+                return INF_ROOTS; // бесконечное кол-во корней
             else
-                *x1 = *x2 = -c/b;
-                return 1;
+                return 0; // нет корней
         }
         else
-        {
-            int d = b*b - 4*a*c;
-            if (d < 0)
-                return 0;
-            else if (d == 0)
-                {
-                *x1 = *x2 = -b/(2*a);
-                return 1;
-                }
-            else if (d > 0)
-                {
-                *x1 = (-b + sqrt(b*b - 4*a*c))/(2*a);
-                *x2 = (-b - sqrt(b*b - 4*a*c))/(2*a);
-                return 2;
-                }
-        }
+            *x1 = *x2 = -c/b;
+            return 1;
     }
-
-
-int main(void)
+    else
     {
-        double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
-
-        printf("Введите коэффициенты a, b и c через пробел: ");
-        scanf("%lg %lg %lg", &a, &b, &c);
-
-        int nRoots = solver(a, b, c, &x1, &x2);
-
-        switch (nRoots){
-            case 0: printf("Корней нет");
-                    break;
-            case 1: printf("%lg", x1);
-                    break;
-            case 2: printf("%lg %lg", x1, x2);
-                    break;
-            case INF_ROOTS: printf("Любое число");
-                            break;
-            default: printf("Где-то ошибка");
-                     return 1;
-        }
-
-        return 0;
+        double d = b*b - 4*a*c;
+        if (d < 0)
+            return 0;
+        else if (is_null(d))
+            {
+            *x1 = *x2 = -b/(2*a);
+            return 1;
+            }
+        else if (d > 0)
+            {
+            *x1 = (-b + sqrt(b*b - 4*a*c))/(2*a);
+            *x2 = (-b - sqrt(b*b - 4*a*c))/(2*a);
+            return 2;
+            }
     }
+    return 1;
+}
+
+void output(const int nRoots, const double x1, const double x2)
+{
+    switch (nRoots){
+        case 0: printf("Корней нет \n");
+                break;
+        case 1: printf("%lf \n", x1);
+                break;
+        case 2: printf("%lf %lf \n", x1, x2);
+                break;
+        case INF_ROOTS: printf("Любое число \n");
+                        break;
+        default: fprintf(stderr, "Где-то ошибка \n");
+    }
+}
+
+void input(double* a, double* b, double* c)
+{
+    printf("Введите коэффициенты a, b, c: \n");
+
+    int accepted_values = scanf("%lf %lf %lf", a, b, c);
+    while (accepted_values != 3)
+    {
+        printf("Введите коэффициенты a, b, c: \n");
+        clear_std_out();
+        accepted_values = scanf("%lf %lf %lf", a, b, c);
+    }
+}
+
+void clear_std_out()
+{
+    int symbol = getchar();
+    while (symbol != '\n' && symbol != EOF){
+        symbol = getchar();
+    }
+}
 
 
-
-
-
-
+// enum
+// 93 строчка fprintf
+// 82 строчка void const обьявить переменные
+// lg -> lf
+// структуры
+// assert
+// input функция
 
