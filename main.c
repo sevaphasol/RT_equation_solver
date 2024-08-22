673ddc2fbe90f97859b3e6a192ef9fc372c26198
sevaphasol <sevsol2512@gmail.com>
2024-08-22 21:45:42 +0300

    Added doxygen

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
+/*!
+    \file
+    \brief This file is main in this project
+
+    It solves quad equation
+*/
+
 #include <stdio.h>
 #include <TXLib.h>
 #include <math.h>
@@ -5,60 +12,113 @@
 #include <string.h>
 
 
-//---------
-//! @param [in] coeffs
-//! @param [our] roots
-//!
-//!
-//!
-//---------
-// !!!! bbb_aaaa_ssss (snake)
-// !!!! bbbAaaaSsss (Camel)
-// !!!! BbbAaaaSsss (Pascal)
-
+/// Number of quad equation roots
 enum NumberOfRoots
 {
-    NOT_INITIALIZED = -2,
-    INF = -1,
-    ZERO = 0,
-    ONE = 1,
-    TWO = 2
+    NOT_INITIALIZED = -2, //equation hasn't been made
+    INF = -1, //equation has infinte roots
+    ZERO = 0, // equation has 0 roots
+    ONE = 1, // equation has 1 root
+    TWO = 2, // equation has 2 roots
 };
 
-/*
-enum Flags
-{
-    HELP = '--help',
-};
+/// Accuracy of double comparison
+const double EPSILON = 0.000001; 
+
+/// Structure for coeffitients of quad equation
+typedef struct Coefficients {
+	double a; ///< 1st coeffitient a*x^2
+	double b; ///< 2nd coeffitient b*x
+	double c; ///< 3d coeffitient c
+} Coefficients;
+
+/// Structure for roots of quad equation
+typedef struct Roots {
+    NumberOfRoots amount_of_roots; ///< amount of roots
+    double x1; ///< first root
+    double x2; ///< second root
+} Roots;
+
+/// Structure for testing programm
+typedef struct TestQuad { 
+	int number_of_test; ///< number of test 
+	Coefficients coeffs; /// coeffitients of equation
+	Roots right_roots; /// right roots of equation 
+} TestQuad;
+
+/*!
+Solves quad equation
+\param[out] roots quad equation roots
+\param[in] coeffs quad equation coeffititens
 */
-
-
-const double EPSILON = 0.000001; /* for double_compare */
-
-typedef struct Coefficients { double a; double b; double c; } Coefficients;
-
-typedef struct Roots { NumberOfRoots amount_of_roots; double x1; double x2; } Roots;
-
-typedef struct TestQuad { int number_of_test; Coefficients coeffs; Roots right_roots; } TestQuad;
-
 void quad_solver(const Coefficients coeffs, Roots* const roots);
+
+/*!
+Displays quad equation roots
+\param[in] roots quad equation roots
+*/
 void roots_output(const Roots roots);
+/*!
+Receives quad equation coeffitients 
+\param[out] coeffs quad equation coeffs
+*/
 void coeffs_input(Coefficients* const coeffs);
-void coeffs_cmd_input(int argc, char * argv[], Coefficients* const coeffs);
+/*!
+Checks input flags in console
+\param[out] only_test no input data, only tests if true 
+\param[out] do_test turn on testing if true
+\param[in] argc
+\param[in] argv
+*/
 void flags_input(int argc, char * argv[], bool *only_test, bool *do_test);
+/*!
+Print help flag information
+*/
 void flag_help();
+/*!
+Turn off input data
+\param[out] only_test no input data, only tests if true 
+*/
 void flag_only_test(bool *only_test);
+/*!
+Turn on testing
+\param[out] do_test turn on testing if true
+*/
 void flag_do_test(bool *do_test);
-void clear_std_out();
+/*!
+Clears stdout for scanf
+*/
+void clear_stdout();
+/*!
+Compares two double
+\param[in] x 1st number
+\param[in] y 2nd number
+\return true if equal else false
+*/
 int double_compare(const double x, const double y);
+/*!
+Test sollving of quad equation
+\param[in] test equation, contains coefficients and right roots
+\return 1 if test is ok else 0
+*/
 int quad_solver_test(const TestQuad test);
+/*!
+Checks if quad equation solved correctly
+\param[in] roots roots from solving
+\param[in] right_roots expected roots
+\return true if quad equation solved correctly else false
+*/
 bool is_quad_solved_incorrectly(const Roots roots, const Roots right_roots);
+/*!
+Do many tests
+\return true if test is ok else false
+*/
 bool quad_solver_testing();
 
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-    Coefficients coeffs = {0, 0, 0};
+	Coefficients coeffs = {0, 0, 0};
     Roots roots = {NOT_INITIALIZED, 0, 0};
     bool only_test = false;
     bool do_test = false;
@@ -67,8 +127,6 @@ int main(int argc, char * argv[])
 
     if (!only_test)
     {
-        /* coeffs_cmd_input(argc, argv, &coeffs); */
-
         coeffs_input(&coeffs);
 
         quad_solver(coeffs, &roots);
@@ -144,16 +202,21 @@ void quad_solver(const Coefficients coeffs, Roots* const roots)
 void roots_output(const Roots roots)
 {
     switch (roots.amount_of_roots){
-        case ZERO: printf("Корней нет \n");
-                break;
-        case ONE: printf("%lf \n", roots.x1);
-                break;
-        case TWO: printf("%lf %lf \n", roots.x1, roots.x2);
-                break;
-        case INF: printf("Любое число \n");
-                break;
+        case ZERO: 
+			printf("Корней нет \n");
+            break;
+        case ONE: 
+			printf("%lf \n", roots.x1);
+			break;
+        case TWO: 
+			printf("%lf %lf \n", roots.x1, roots.x2);
+            break;
+        case INF: 
+			printf("Любое число \n");
+            break;
         case NOT_INITIALIZED:
-        default: fprintf(stderr, "Где-то ошибка \n");
+        default: 
+			fprintf(stderr, "Где-то ошибка \n");
     }
 }
 
@@ -165,26 +228,13 @@ void coeffs_input(Coefficients* const coeffs)
         printf("Введите коэффициенты a, b, c: \n");
         int accepted_values = scanf("%lf %lf %lf", &(coeffs->a), &(coeffs->b), &(coeffs->c));
         if (accepted_values != 3)
-            clear_std_out();
+            clear_stdout();
         else
             break;
     }
 }
 
-/*
-void coeffs_cmd_input(int argc, char * argv[], Coefficients* const coeffs)
-{
-    printf("Количество аргументов, указанных в командной строке: %d\n", argc - 1);
-    for (int count = 1; count < argc; count++)
-        printf("%d: %s\n", count, argv[count]);
-    printf("\n");
-    //coeffs->a = argv[1];
-    //coeffs->b = (double)argv[2];
-    //coeffs->c = (double)argv[3];
-}
-*/
-
-void flags_input(int argc, char * argv[], bool *only_test, bool *do_test)
+void flags_input(int argc, char *argv[], bool *only_test, bool *do_test)
 {
     for (int count = 1; count < argc; count++)
     {
@@ -221,10 +271,11 @@ void flag_do_test(bool *do_test)
     *do_test = true;
 }
 
-void clear_std_out()
+void clear_stdout()
 {
     int symbol = getchar();
-    while (symbol != '\n' && symbol != EOF){
+    while (symbol != '\n' && symbol != EOF)
+	{
         symbol = getchar();
     }
 }
@@ -273,8 +324,3 @@ bool quad_solver_testing()
     }
     return false;
 }
-
-
-
-
-// test bool возвращать
